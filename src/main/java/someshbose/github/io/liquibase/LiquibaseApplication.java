d1cd52c84ce79acf1c269eb73596a3f82910c01d
Someshbose <iamsomeshbose@gmail.com>
2021-04-18 22:25:53 +0530

    cache related changes

--- src/main/java/someshbose/github/io/liquibase/LiquibaseApplication.java
+++ src/main/java/someshbose/github/io/liquibase/LiquibaseApplication.java
@@ -2,8 +2,10 @@ package someshbose.github.io.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LiquibaseApplication {
 
 	public static void main(String[] args) {
